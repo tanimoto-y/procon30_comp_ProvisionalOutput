20e5eebb4be48110097b8b8236e8cf780452ee2a
tanimoto-y <s16535@salesio-sp.ac.jp>
2019-05-04 00:26:46 +0900

    ターン制の導入、および操作履歴の導入に向けた準備

---
+++ projects/ProvisionalOutput/src/button.cpp
@@ -0,0 +1,8 @@
+//
+//  button.cpp
+//  empty
+//
+//  Created by 谷本 陽祐 on 2019/04/29.
+//
+
+#include "button.hpp"
---
+++ projects/ProvisionalOutput/src/button.hpp
@@ -0,0 +1,13 @@
+//
+//  button.hpp
+//  empty
+//
+//  Created by 谷本 陽祐 on 2019/04/29.
+//
+
+#ifndef button_hpp
+#define button_hpp
+
+#include <stdio.h>
+
+#endif /* button_hpp */
